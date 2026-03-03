a4b17b1f4ef583316034e1eb9e6782075d5564cb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-02 22:53:56 -0300

    feat

---
+++ src/main/java/br/com/fiap/study_apir/PingController.java
@@ -0,0 +1,29 @@
+package br.com.fiap.study_apir;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping()
+public class PingController {
+
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+
+
+    @GetMapping("/rota1")
+    public String rota1(){
+        return "rota1";
+    }
+
+    @GetMapping("/rota2")
+    public String rota2(){
+        return "rota2";
+    }
+
+
+}
